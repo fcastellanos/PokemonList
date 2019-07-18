16ddea56d231961ce691c6c14f280fea6672bab8
Fernando Castellanos <fernando.castellanos@gmail.com>
2019-07-17 19:29:05 -0700

    Initial Commit

---
+++ PokemonList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PokemonList
+//
+//  Created by Juan Fernando Castellanos on 7/17/19.
+//  Copyright © 2019 fcastellanos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
